a8b32164c68169ba3ba90588588cf50fe5a51da2
Arsalan Kashkoee <Arsalan.kashkoee@gmail.com>
2022-09-17 16:06:36 +0430

    carousel

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -18,19 +18,43 @@ const Carousel = () => {
   }, [currentImg]);
 
   return (
-    <section className="container flex items-center justify-center h-[500px] w-screen">
-      <div className="carousel w-full h-full  bg-slate-900">
+    <section className="container flex items-center justify-center h-[500px] w-screen ">
+      <div className="w-full h-full">
         <div
           style={{ backgroundImage: `url(${images[currentImg].img})` }}
-          className="carouselInner relative h-full w-full bg-cover bg-center bg-no-repeat flex"
+          className="carouselInner relative h-full w-full bg-cover bg-center bg-no-repeat transition-all ease-in-out duration-1000 rounded-md overflow-hidden"
         >
           {/* bg-gradient */}
           <div className="w-full h-full absolute top-0 left-0 bg-gradient-to-r from-black/80"></div>
 
           {/* title-subtitle */}
-          <div className="text-white absolute bottom-11 left-1/2 opacity-60 flex flex-col items-center gap-3">
+          <div className="text-white opacity-60 flex flex-col gap-3 mb-3 absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
             <span className="">{images[currentImg].title}</span>
             <span className="">{images[currentImg].subtitle}</span>
+          </div>
+
+          {/* circles */}
+          <div className="flex absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
+            <div
+              onClick={() => setCurrentImage(0)}
+              className={`w-3 h-3 rounded-full bg-white transition-all ease-in-out duration-1000 mr-3 cursor-pointer  ${
+                currentImg === 0 ? "opacity-100" : "opacity-40"
+              }`}
+            ></div>
+
+            <div
+              onClick={() => setCurrentImage(1)}
+              className={`w-3 h-3 rounded-full bg-white transition-all ease-in-out duration-1000 mr-3 cursor-pointer  ${
+                currentImg === 1 ? "opacity-100" : "opacity-40"
+              }`}
+            ></div>
+
+            <div
+              onClick={() => setCurrentImage(2)}
+              className={`w-3 h-3 rounded-full bg-white transition-all ease-in-out duration-1000 mr-3 cursor-pointer  ${
+                currentImg === 2 ? "opacity-100" : "opacity-40"
+              }`}
+            ></div>
           </div>
 
           {/* button-group */}
